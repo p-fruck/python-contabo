3547d0e1e6aae8dbcee87884fe211e5f4e4eaeeb
p-fruck <p-fruck@users.noreply.github.com>
2022-01-13 15:39:30 +0000

    update to latest API spec

--- pfruck_contabo/configuration.py
+++ pfruck_contabo/configuration.py
@@ -240,5 +240,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0.0\n"\
-               "SDK Package Version: 1.0.0".\
+               "SDK Package Version: 1.0.1".\
                format(env=sys.platform, pyversion=sys.version)

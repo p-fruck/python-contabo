214ef3301035cc8d58892861e811755fa4f15f9e
Philipp Fruck <dev@p-fruck.de>
2022-01-29 18:22:48 +0100

    prepare v1.3.2

--- pfruck_contabo/api_client.py
+++ pfruck_contabo/api_client.py
@@ -72,7 +72,7 @@ class ApiClient(object):
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/1.3.1/python'
+        self.user_agent = 'Swagger-Codegen/1.3.2/python'
 
     def __del__(self):
         self.pool.close()

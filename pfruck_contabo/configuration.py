894f881867f03a6c68342b2b6d1f0957ea4c8fa7
Philipp Fruck <dev@p-fruck.de>
2022-01-31 21:24:21 +0100

    prepare v1.4.2

--- pfruck_contabo/configuration.py
+++ pfruck_contabo/configuration.py
@@ -240,5 +240,5 @@ class Configuration(six.with_metaclass(TypeWithDefault, object)):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0.0\n"\
-               "SDK Package Version: 1.3.2".\
+               "SDK Package Version: 1.4.2".\
                format(env=sys.platform, pyversion=sys.version)
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "pfruck_contabo"
-VERSION = "1.3.2"
+VERSION = "1.4.2"
 # To install the library, run the following
 #
 # python setup.py install

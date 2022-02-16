77f9536be908218df3b91faf6ff50b056ea05319
Philipp Fruck <dev@p-fruck.de>
2022-02-16 19:58:31 +0100

    prepare v1.5.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "pfruck_contabo"
-VERSION = "1.4.3"
+VERSION = "1.5.0"
 # To install the library, run the following
 #
 # python setup.py install

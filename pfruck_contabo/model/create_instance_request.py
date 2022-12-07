4dbdddcc8429bc39895c7cf81c398b2147531b4e
p-fruck <p-fruck@users.noreply.github.com>
2022-12-07 18:21:51 +0000

    update to latest API spec

--- pfruck_contabo/model/create_instance_request.py
+++ pfruck_contabo/model/create_instance_request.py
@@ -62,6 +62,7 @@ class CreateInstanceRequest(ModelNormal):
             'US-WEST': "US-west",
             'SIN': "SIN",
             'UK': "UK",
+            'AUS': "AUS",
         },
         ('license',): {
             'PLESKHOST': "PleskHost",
@@ -175,7 +176,7 @@ class CreateInstanceRequest(ModelNormal):
         Keyword Args:
             image_id (str): ImageId to be used to setup the compute instance. Default is Ubuntu 20.04. defaults to "db1409d2-ed92-4f2f-978e-7b2fa4a1ec90"  # noqa: E501
             product_id (str): Default is V1. defaults to "V1"  # noqa: E501
-            region (str): Instance Region where the compute instance should be located. Default is EU. defaults to "EU", must be one of ["EU", "US-central", "US-east", "US-west", "SIN", "UK", ]  # noqa: E501
+            region (str): Instance Region where the compute instance should be located. Default is EU. defaults to "EU", must be one of ["EU", "US-central", "US-east", "US-west", "SIN", "UK", "AUS", ]  # noqa: E501
             period (int): Initial contract period in months. Available periods are: 1, 3, 6 and 12 months. Default to 1 month. defaults to 1  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be
@@ -280,7 +281,7 @@ class CreateInstanceRequest(ModelNormal):
         Keyword Args:
             image_id (str): ImageId to be used to setup the compute instance. Default is Ubuntu 20.04. defaults to "db1409d2-ed92-4f2f-978e-7b2fa4a1ec90"  # noqa: E501
             product_id (str): Default is V1. defaults to "V1"  # noqa: E501
-            region (str): Instance Region where the compute instance should be located. Default is EU. defaults to "EU", must be one of ["EU", "US-central", "US-east", "US-west", "SIN", "UK", ]  # noqa: E501
+            region (str): Instance Region where the compute instance should be located. Default is EU. defaults to "EU", must be one of ["EU", "US-central", "US-east", "US-west", "SIN", "UK", "AUS", ]  # noqa: E501
             period (int): Initial contract period in months. Available periods are: 1, 3, 6 and 12 months. Default to 1 month. defaults to 1  # noqa: E501
             _check_type (bool): if True, values for parameters in openapi_types
                                 will be type checked and a TypeError will be

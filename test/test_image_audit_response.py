4096dc35991c2e628b558c0401d8cb6350428db6
p-fruck <p-fruck@users.noreply.github.com>
2023-07-19 18:18:56 +0000

    update to latest API spec

--- test/test_image_audit_response.py
+++ test/test_image_audit_response.py
@@ -14,10 +14,10 @@ import unittest
 import pfruck_contabo
 from pfruck_contabo.model.image_audit_response_data import ImageAuditResponseData
 from pfruck_contabo.model.image_audit_response_links import ImageAuditResponseLinks
-from pfruck_contabo.model.list_user_response_pagination import ListUserResponsePagination
+from pfruck_contabo.model.list_instances_response_pagination import ListInstancesResponsePagination
 globals()['ImageAuditResponseData'] = ImageAuditResponseData
 globals()['ImageAuditResponseLinks'] = ImageAuditResponseLinks
-globals()['ListUserResponsePagination'] = ListUserResponsePagination
+globals()['ListInstancesResponsePagination'] = ListInstancesResponsePagination
 from pfruck_contabo.model.image_audit_response import ImageAuditResponse
 
 

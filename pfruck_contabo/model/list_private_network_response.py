bf46a6f01e8c4b37dff3ed1be198e0eb34468c80
p-fruck <p-fruck@users.noreply.github.com>
2023-10-02 18:19:41 +0000

    update to latest API spec

--- pfruck_contabo/model/list_private_network_response.py
+++ pfruck_contabo/model/list_private_network_response.py
@@ -30,12 +30,12 @@ from pfruck_contabo.exceptions import ApiAttributeError
 
 
 def lazy_import():
+    from pfruck_contabo.model.list_instances_response_pagination import ListInstancesResponsePagination
     from pfruck_contabo.model.list_private_network_response_data import ListPrivateNetworkResponseData
     from pfruck_contabo.model.list_private_network_response_links import ListPrivateNetworkResponseLinks
-    from pfruck_contabo.model.list_user_response_pagination import ListUserResponsePagination
+    globals()['ListInstancesResponsePagination'] = ListInstancesResponsePagination
     globals()['ListPrivateNetworkResponseData'] = ListPrivateNetworkResponseData
     globals()['ListPrivateNetworkResponseLinks'] = ListPrivateNetworkResponseLinks
-    globals()['ListUserResponsePagination'] = ListUserResponsePagination
 
 
 class ListPrivateNetworkResponse(ModelNormal):
@@ -91,7 +91,7 @@ class ListPrivateNetworkResponse(ModelNormal):
         """
         lazy_import()
         return {
-            'pagination': (ListUserResponsePagination,),  # noqa: E501
+            'pagination': (ListInstancesResponsePagination,),  # noqa: E501
             'data': ([ListPrivateNetworkResponseData],),  # noqa: E501
             'links': (ListPrivateNetworkResponseLinks,),  # noqa: E501
         }
@@ -118,7 +118,7 @@ class ListPrivateNetworkResponse(ModelNormal):
         """ListPrivateNetworkResponse - a model defined in OpenAPI
 
         Args:
-            pagination (ListUserResponsePagination):
+            pagination (ListInstancesResponsePagination):
             data ([ListPrivateNetworkResponseData]):
             links (ListPrivateNetworkResponseLinks):
 
@@ -211,7 +211,7 @@ class ListPrivateNetworkResponse(ModelNormal):
         """ListPrivateNetworkResponse - a model defined in OpenAPI
 
         Args:
-            pagination (ListUserResponsePagination):
+            pagination (ListInstancesResponsePagination):
             data ([ListPrivateNetworkResponseData]):
             links (ListPrivateNetworkResponseLinks):
 

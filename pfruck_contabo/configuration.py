77b7c8900a7b475675ab846df8194266d0916961
p-fruck <p-fruck@users.noreply.github.com>
2023-10-11 19:53:46 +0000

    nightly API update

--- pfruck_contabo/configuration.py
+++ pfruck_contabo/configuration.py
@@ -389,7 +389,7 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0.0\n"\
-               "SDK Package Version: 1.10.0".\
+               "SDK Package Version: 1.11.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):

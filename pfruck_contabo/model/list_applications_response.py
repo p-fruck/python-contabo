b6957d91b65eb11f6237f4f101ba849b59edcfd0
Philipp Fruck <p-fruck@users.noreply.github.com>
2023-12-22 18:18:32 +0000

    nightly API update

--- pfruck_contabo/model/list_applications_response.py
+++ pfruck_contabo/model/list_applications_response.py
@@ -32,10 +32,10 @@ from pfruck_contabo.exceptions import ApiAttributeError
 def lazy_import():
     from pfruck_contabo.model.application_response import ApplicationResponse
     from pfruck_contabo.model.list_applications_response_links import ListApplicationsResponseLinks
-    from pfruck_contabo.model.list_instances_response_pagination import ListInstancesResponsePagination
+    from pfruck_contabo.model.list_user_response_pagination import ListUserResponsePagination
     globals()['ApplicationResponse'] = ApplicationResponse
     globals()['ListApplicationsResponseLinks'] = ListApplicationsResponseLinks
-    globals()['ListInstancesResponsePagination'] = ListInstancesResponsePagination
+    globals()['ListUserResponsePagination'] = ListUserResponsePagination
 
 
 class ListApplicationsResponse(ModelNormal):
@@ -91,7 +91,7 @@ class ListApplicationsResponse(ModelNormal):
         """
         lazy_import()
         return {
-            'pagination': (ListInstancesResponsePagination,),  # noqa: E501
+            'pagination': (ListUserResponsePagination,),  # noqa: E501
             'data': ([ApplicationResponse],),  # noqa: E501
             'links': (ListApplicationsResponseLinks,),  # noqa: E501
         }
@@ -118,7 +118,7 @@ class ListApplicationsResponse(ModelNormal):
         """ListApplicationsResponse - a model defined in OpenAPI
 
         Args:
-            pagination (ListInstancesResponsePagination):
+            pagination (ListUserResponsePagination):
             data ([ApplicationResponse]):
             links (ListApplicationsResponseLinks):
 
@@ -211,7 +211,7 @@ class ListApplicationsResponse(ModelNormal):
         """ListApplicationsResponse - a model defined in OpenAPI
 
         Args:
-            pagination (ListInstancesResponsePagination):
+            pagination (ListUserResponsePagination):
             data ([ApplicationResponse]):
             links (ListApplicationsResponseLinks):
 
--- pfruck_contabo/model/list_secret_audit_response.py
+++ pfruck_contabo/model/list_secret_audit_response.py
@@ -30,11 +30,11 @@ from pfruck_contabo.exceptions import ApiAttributeError
 
 
 def lazy_import():
-    from pfruck_contabo.model.list_instances_response_pagination import ListInstancesResponsePagination
     from pfruck_contabo.model.list_secret_audit_response_links import ListSecretAuditResponseLinks
+    from pfruck_contabo.model.list_user_response_pagination import ListUserResponsePagination
     from pfruck_contabo.model.secret_audit_response import SecretAuditResponse
-    globals()['ListInstancesResponsePagination'] = ListInstancesResponsePagination
     globals()['ListSecretAuditResponseLinks'] = ListSecretAuditResponseLinks
+    globals()['ListUserResponsePagination'] = ListUserResponsePagination
     globals()['SecretAuditResponse'] = SecretAuditResponse
 
 
@@ -91,7 +91,7 @@ class ListSecretAuditResponse(ModelNormal):
         """
         lazy_import()
         return {
-            'pagination': (ListInstancesResponsePagination,),  # noqa: E501
+            'pagination': (ListUserResponsePagination,),  # noqa: E501
             'data': ([SecretAuditResponse],),  # noqa: E501
             'links': (ListSecretAuditResponseLinks,),  # noqa: E501
         }
@@ -118,7 +118,7 @@ class ListSecretAuditResponse(ModelNormal):
         """ListSecretAuditResponse - a model defined in OpenAPI
 
         Args:
-            pagination (ListInstancesResponsePagination):
+            pagination (ListUserResponsePagination):
             data ([SecretAuditResponse]):
             links (ListSecretAuditResponseLinks):
 
@@ -211,7 +211,7 @@ class ListSecretAuditResponse(ModelNormal):
         """ListSecretAuditResponse - a model defined in OpenAPI
 
         Args:
-            pagination (ListInstancesResponsePagination):
+            pagination (ListUserResponsePagination):
             data ([SecretAuditResponse]):
             links (ListSecretAuditResponseLinks):
 
